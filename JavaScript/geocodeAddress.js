438d9cfe649dd32fa4aaeb4273a45983021ba870
Carlo Feniello <carlofeniello@hotmail.it>
2020-03-31 14:21:59 +0200

    Create geocodeAddress.js

---
+++ JavaScript/geocodeAddress.js
@@ -0,0 +1,16 @@
+#Google Maps API - traduce indirizzo in coordinate
+function geocodeAddress(geocoder, resultsMap) {
+
+    geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+        let newLat = results[0].geometry.location.lat();
+        let newLng = results[0].geometry.location.lng();
+        
+        console.log(newLat);
+        console.log(newLng);
+
+    } else {
+        alert('Ricerca indirizzo fallita. causa: ' + status);
+    }
+    });
+}
